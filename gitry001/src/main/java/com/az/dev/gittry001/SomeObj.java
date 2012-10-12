ac95124cb103b9057792e8e7d31740cc84577a2f
azinczuk <azinczuk@gmail.com>
2012-10-12 23:27:16 +0300

    Update gitry001/src/main/java/com/az/dev/gittry001/SomeObj.java

--- gitry001/src/main/java/com/az/dev/gittry001/SomeObj.java
+++ gitry001/src/main/java/com/az/dev/gittry001/SomeObj.java
@@ -2,9 +2,9 @@ package com.az.dev.gittry001;
 
 public class SomeObj {
 
-		private String someObjName;
+	private String someObjName;
 
-		public SomeObj(String someObjName) {
-			this.someObjName = someObjName;
-		}
+	public SomeObj(String someObjName) {
+		this.someObjName = someObjName;
+	}
 }
