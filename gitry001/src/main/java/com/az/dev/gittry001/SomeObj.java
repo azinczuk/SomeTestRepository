1307fd86c7274ffec2009d0fb8ab5cd196968076
azinczuk <azinczuk@gmail.com>
2012-10-12 22:39:02 +0200

    Fix

--- gitry001/src/main/java/com/az/dev/gittry001/SomeObj.java
+++ gitry001/src/main/java/com/az/dev/gittry001/SomeObj.java
@@ -3,12 +3,14 @@ package com.az.dev.gittry001;
 public class SomeObj {
 
 	private String someObjName;
+	private String someObjSecondName;
 
-	public SomeObj(String someObjName) {
+	public SomeObj(String someObjName, String someObjSecondName) {
 		this.someObjName = someObjName;
+		this.someObjSecondName = someObjSecondName;
 	}
 	
 	public String toString(){
-		return "someObjName:" + someObjName;
+		return "someObjName:" + someObjName +" someObjSecondName:"+someObjSecondName;
 	}
 }
