429444f79d896be3e43b3c5566deb2aeeafaedd1
azinczuk <azinczuk@gmail.com>
2012-10-12 23:22:22 +0300

    Update gitry002/src/main/java/com/az/dev/gittry002/SomeObj.java

--- gitry002/src/main/java/com/az/dev/gittry002/SomeObj.java
+++ gitry002/src/main/java/com/az/dev/gittry002/SomeObj.java
@@ -2,6 +2,9 @@ package com.az.dev.gittry002;
 
 public class SomeObj {
 
-	public SomeObj() {
+	private String someObjName;	
+
+	public SomeObj(String someObjName) {
+		this.someObjName = someObjName; 
 	}
 }
